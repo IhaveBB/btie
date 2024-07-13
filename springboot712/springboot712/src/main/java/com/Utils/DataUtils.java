17fead062d882944bd43dabf64dbd48416e90372
IhaveBB <IhaveBB@outlook.com>
2024-07-13 14:28:48 +0800

    最终完成

--- springboot712/springboot712/src/main/java/com/Utils/DataUtils.java
+++ springboot712/springboot712/src/main/java/com/Utils/DataUtils.java
@@ -2,6 +2,7 @@ package com.Utils;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Created by IhaveBB on 2024/7/13 8:47
@@ -20,8 +21,19 @@ public class DataUtils {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 		return now.format(formatter);
 	}
+	/**
+	 * 获取前一天的日期并格式化
+	 *
+	 * @return 前一天日期的格式化字符串
+	 */
+	public static String getPreviousDateFormatted() {
+		LocalDate yesterday = LocalDate.now().minus(1, ChronoUnit.DAYS);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+		return yesterday.format(formatter);
+	}
 
 	public static void main(String[] args) {
 		System.out.println(getCurrentDateFormatted());
+		System.out.println(getPreviousDateFormatted());
 	}
 }
